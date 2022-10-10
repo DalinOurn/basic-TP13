1622139bcd07cf2546f52f5d915058ea36899700
DalinOurn <ourndalin111@gmail.com>
2022-10-10 13:48:08 +0700

    auth mongo

--- Server/configs/db.js
+++ Server/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 module.exports = async() => {
     try {
-        await mongoose.connect('mongodb+srv://admin:<password>@cluster0.9wjw9ff.mongodb.net/product');
+        await mongoose.connect('mongodb+srv://admin:2222@cluster0.9wjw9ff.mongodb.net/product');
         console.log("MongoDB connected><");
     } catch (error) {
         console.log("MongoDB err: ", error);
